e73916eeecb780e3f3d5bf7e7239814bfde930af
Mariano Cano <mariano.cano@gmail.com>
2021-12-02 13:03:38 -0800

    Add copyright.

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+// Copyright 2021 Smallstep Labs, Inc. All rights reserved. Licensed to the
+// Apache Software Foundation (ASF) under one or more contributor license
+// agreements; and to You under the Apache License, Version 2.0.
+
 package main
 
 import (
